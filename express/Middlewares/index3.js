cf43a34bff5160f661896c27669ba4aa12fbb3f7
AustinMusuya <amusuya57@gmail.com>
2024-10-13 06:04:43 +0300

    created custom middleware 'logger' to log the req methods and url to the console

--- express/Middlewares/index3.js
+++ express/Middlewares/index3.js
@@ -3,10 +3,21 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+  console.log(`Request Method: ${req.method}`);
+  console.log(`Request URL: ${req.url}`);
+  next();
+};
+// Middlewares
+
 app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
