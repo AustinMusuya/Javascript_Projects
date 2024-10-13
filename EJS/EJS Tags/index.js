bad58879eb1c3bb09fef1724d6ffc91a80876406
austinmusuya <amusuya57@gmail.com>
2024-10-13 14:25:51 +0300

    add folder EJS Tags and files to practice ejs tags in javascript and html

---
+++ EJS/EJS Tags/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const port = 3000;
+
+// Middleware
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<strong>This is some strong text</strong>",
+  };
+  res.render("index.ejs", data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
