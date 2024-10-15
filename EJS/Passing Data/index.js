353797baf347266c43dcde8680d3e6f6a53236be
AustinMusuya <amusuya57@gmail.com>
2024-10-15 09:34:38 +0300

    add morgan middleware to index.js file

--- EJS/Passing Data/index.js
+++ EJS/Passing Data/index.js
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+// Middleware
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
