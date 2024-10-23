b031505007f065928c5e6eb823207dee746e301f
AustinMusuya <amusuya57@gmail.com>
2024-10-23 06:04:51 +0300

    ass ocde to handle put and patch requests to index.js

--- Build Your API/DIY/index.js
+++ Build Your API/DIY/index.js
@@ -34,10 +34,44 @@ app.get("/filter", (req, res) => {
 });
 
 //4. POST a new joke
+app.post("/jokes", (req, res) => {
+  const requestedJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  jokes.push(requestedJoke);
+  console.log(jokes.slice(-1));
+  res.json(requestedJoke);
+});
 
 //5. PUT a joke
+app.put("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const replacement = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  const index = jokes.findIndex((joke) => joke.id === id);
+  jokes[index] = replacement;
+  res.json(replacement);
+});
 
 //6. PATCH a joke
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const existingJoke = jokes.find((joke) => joke.id === id);
+  const replacement = {
+    id: id,
+    jokeText: req.body.text || existingJoke.jokeText,
+    jokeType: req.body.type || existingJoke.jokeType,
+  };
+  const index = jokes.findIndex((joke) => joke.id === id);
+  jokes[index] = replacement;
+  console.log(jokes[index]);
+  res.json(replacement);
+});
 
 //7. DELETE Specific joke
 
