35b6fbbafb0fe2474157f13afc0d43a1a5bbde04
AustinMusuya <amusuya57@gmail.com>
2024-10-27 09:56:38 +0300

    solve code challenge in folder "Country Flags Quiz" file `index.js`

--- PostgreSQL/Country Flags Quiz/index.js
+++ PostgreSQL/Country Flags Quiz/index.js
@@ -1,14 +1,39 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
 
 let totalCorrect = 0;
 
+// set up Db connection
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Chrislil1!",
+  port: 5432,
+});
+
+db.connect();
+
+let quiz = [];
+
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.log(err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(morgan("dev"));
 
 let currentQuestion = {};
 
@@ -22,9 +47,10 @@ app.get("/", (req, res) => {
 
 // POST a new post
 app.post("/submit", (req, res) => {
+  console.log(req.body);
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
@@ -38,10 +64,15 @@ app.post("/submit", (req, res) => {
   });
 });
 
-function nextQuestion() {
+const nextQuestion = async () => {
   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
   currentQuestion = randomCountry;
-}
+};
+
+// function nextQuestion() {
+//   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+//   currentQuestion = randomCountry;
+// }
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
