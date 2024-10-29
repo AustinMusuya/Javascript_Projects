eaf2ea885ee85b4195fca1f4e4caa0f11175c46b
AustinMusuya <amusuya57@gmail.com>
2024-10-29 18:22:40 +0300

    cofigure code in index.js to handle get route to display countries visited and post route to update database values

--- PostgreSQL/Travel Tracker/index.js
+++ PostgreSQL/Travel Tracker/index.js
@@ -1,14 +1,65 @@
 import express from "express";
 import bodyParser from "body-parser";
+import morgan from "morgan";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(morgan("dev"));
+
+// Set up db connection
+
+const db = new pg.Client({
+  user: "postgres",
+  database: "world",
+  host: "localhost",
+  password: "Chrislil1!",
+  port: 5432,
+});
+
+db.connect();
+
+const checkVisited = async () => {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(result.rows);
+  return countries;
+};
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  const countries = await checkVisited();
+  const totalCount = countries.length;
+
+  console.log(countries);
+  res.render("index.ejs", { countries: countries, total: totalCount });
+});
+
+// add a country that has been visited through post request
+app.post("/add", async (req, res) => {
+  const countryCode = req.body.country.toUpperCase();
+  console.log(countryCode);
+  // query to check if request is already in the table
+  const checker = await db.query(
+    "SELECT country_code FROM visited_countries WHERE country_code =$1",
+    [countryCode]
+  );
+  console.log(checker.rows);
+  try {
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
