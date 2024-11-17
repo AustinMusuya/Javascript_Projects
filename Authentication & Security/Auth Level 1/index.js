629461b0c39466d62e4e38235c849a26a50870b2
AustinMusuya <amusuya57@gmail.com>
2024-11-17 19:55:33 +0300

    install npm packages

--- Authentication & Security/Auth Level 1/index.js
+++ Authentication & Security/Auth Level 1/index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 app.use(express.static("public"));
+
+// set up Db connection
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Chrislil1!",
+  port: 5432,
+});
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -19,9 +32,13 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  console.log(req.body);
+});
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
